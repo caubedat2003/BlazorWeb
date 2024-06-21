edaadc24e2a9079d44d7c20a060bbe7ff73b2828
Anh Dat <anh23dat.truong@gmail.com>
2024-06-21 15:14:33 +0700

    dentistry

---
+++ TodoList.Models/TreatProgressRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class TreatProgressRequest
+    {
+        public DateTime CreatedDate { get; set; }
+        public string Doctor { get; set; }
+        public DateTime DateStart { get; set; }
+        public int DateEnd { get; set; }
+        public string RecommendSource { get; set; }
+        public string RecommendPerson { get; set; }
+        public int PicturePeriod { get; set; }
+        public DateTime CSKH { get; set; }
+        public string Notice { get; set; }
+        public string Diagnose { get; set; }
+        public string TreatmentMethod { get; set; }
+    }
+}
